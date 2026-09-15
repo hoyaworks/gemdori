a17bb205ffab9fc031c3a847627281ac9810d464
hoyaworks <hoyaworks@users.noreply.github.com>
2026-09-15 01:11:18 +0000

    deploy: f94ca2e4a919fef3edc5b2ecffd21c8ef2c0e71f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4100362108"
+    serviceWorkerVersion: "4236072285"
   }
 });
